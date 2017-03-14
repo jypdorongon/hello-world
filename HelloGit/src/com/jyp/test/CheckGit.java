92da6ae787b6bcde8db36712df07bcd252162305
jyp <dorongonjaypee@gmail.com>
2017-03-14 16:42:30 +0800

    added more character

--- HelloGit/src/com/jyp/test/CheckGit.java
+++ HelloGit/src/com/jyp/test/CheckGit.java
@@ -4,11 +4,13 @@ public class CheckGit {
 
 	public static void main(String[] args) {
 
-		String[] lotr = {"Frodo", "Samwise", "Gandalf", "Legolas", "Aaragorn"};
+		String[] lotr = {"Frodo", "Samwise", "Gandalf", "Legolas", "Aaragorn", "Boromir", "Faramir"};
 		
-		for(String hero: lotr)
+		for(int i = 0; i < lotr.length; i++)
 		{
-			System.out.println("Welcome " + hero + "!");
+			System.out.println("Welcome " + lotr[i] + "!");
 		}
+		
+		System.out.println("You have no power here! ");
 	}
 }
